cab2a69ad3435d409f39d1c1f437b10639b55b6d
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-12-15 18:13:38 +0200

    Create homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles.css';
+
+const Homepage = () => (
+  <section className="homepage" id="homepage">
+    <h1>Refleksoloji Nedir?</h1>
+    <p>
+      Refleksoloji, insan vücudundaki organların, el ve ayaklarda sonlanan sinir
+      uçlarına bası uygulanarak, çeşitli rahatsızlıklara yönelik destek tedavi
+      sağlanan tamamlayıcı tıp metodu. Refleksoloji ile, belirli baskı
+      noktalarına çeşitli masajlar yaparak organlardaki bozukluklar
+      iyileştirilmeye çalışılır. Ayaklara uygulanan özel masaj hareketleri ile
+      bedenin belli alanlarında toplanan enerjiyi dağıtarak vücudun kendi
+      kendini tedavi etme yeteneğini harekete geçirme yöntemi olarak
+      tanımlanabilir.
+    </p>
+  </section>
+);
+
+export default Homepage;
