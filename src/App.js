39188adcb29096548bc004add234e98e5d59e6bd
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-12-15 18:15:31 +0200

    Update files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Homepage from './components/Homepage';
 
 const App = () => (
   <Router>
     <div className="App">
       <Navigation />
+      <Homepage />
     </div>
   </Router>
 );
