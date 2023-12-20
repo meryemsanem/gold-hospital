e3ab3b29a76167945e5509c88e8b59e74dcbadea
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-12-20 09:52:22 +0200

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>©2023, Gold Hospitals. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
